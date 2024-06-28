fb1c2ac3b953267c745a861ab626fa335efebca1
jfur17 <166375305+jfur17@users.noreply.github.com>
2024-06-28 15:39:50 +0900

    Add files via upload

---
+++ src/orders/orders.router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+
+
+router.route("/:ordersId").get(controller.read).put(controller.update).delete(controller.destroy).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
+
+module.exports = router;
